898c42da2cc09eb3b49b1fe3741b8110c3253b86
Jilles Valk <jvalk@sogyo.nl>
2019-12-02 16:08:06 +0100

    Second part.

--- app/1.hs
+++ app/1.hs
@@ -13,8 +13,7 @@ calcFuel handle fuelSoFar = do
     if (not eof) then do
         maybeInt <- readLine handle
         case maybeInt of
-            (Just moduleMass) -> calcFuel handle 
-                (fuelSoFar + moduleMass `div` 3 -2)
+            (Just moduleMass) -> calcFuel handle $ fuelSoFar + calcFuelThisModule moduleMass
             Nothing -> putStrLn "What was read was not an integer."
     else 
         putStrLn $ "Fuel used: " ++ show fuelSoFar
@@ -23,3 +22,23 @@ readLine :: Handle -> IO (Maybe Int)
 readLine handle = do 
     line <- hGetLine handle
     return (readMay line)
+
+calcFuelThisModule :: Int -> Int
+calcFuelThisModule moduleMass = 
+    let fuelForMass = calcFuelForMass moduleMass
+        fuelForFuel = calcFuelForFuel 0 fuelForMass
+    in fuelForMass + fuelForFuel
+
+calcFuelForFuel :: Int -> Int -> Int
+calcFuelForFuel fuelSoFar fuelMass 
+    | fuelMass <= 0 = fuelSoFar
+    | otherwise = calcFuelForFuel (fuelSoFar + fuelForMass) fuelForMass 
+    where 
+        fuelForMass = calcFuelForMass fuelMass
+
+calcFuelForMass :: Int -> Int
+calcFuelForMass moduleMass 
+    | fuel < 0 = 0
+    | otherwise = fuel
+    where 
+        fuel = moduleMass `div` 3 - 2
