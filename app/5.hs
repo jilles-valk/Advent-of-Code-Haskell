447f4e85c14666a94d5e13c691e636bc82e3325c
Jilles Valk <jvalk@sogyo.nl>
2019-12-09 10:16:58 +0100

    Day five working.

--- app/5.hs
+++ app/5.hs
@@ -5,42 +5,57 @@ main :: IO ()
 main = do 
     handle <- openFile "data/input5.txt" ReadMode
     contents <- hGetContents handle
-    let input = map read $ splitOn "," contents :: [Int]
+    let program = map read $ splitOn "," contents :: [Int]
     putStrLn $ "The output is: " 
-        ++ show (compute input 0)
+        ++ show (compute program 0 [5] [])
 
-compute :: [Int] -> Int -> Int
-compute input index
-    | length input < index = head input
-    | opcode == 99 = head input
-    | opcode == 1 = compute (replaceN input (instructions !! 3) add) endIndex
-    | opcode == 2 = compute (replaceN input (instructions !! 3) multiply) endIndex
-    | opcode == 3 = compute (replaceN input (instructions !! 1) 1) endIndex
-    | opcode == 4 = input !! (instructions !! 1)
-    | otherwise = -1
+compute :: [Int] -> Int -> [Int] -> [Int] -> [Int]
+compute program index input output
+    | length program < index = output
+    | opcode == 99 = output
+    | opcode == 1 = compute (replaceN program (instructions !! 3) add) 
+                        endIndex input output
+    | opcode == 2 = compute (replaceN program (instructions !! 3) multiply) 
+                        endIndex input output
+    | opcode == 3 = compute (replaceN program (instructions !! 1) (head input)) 
+                        endIndex (tail input) output
+    | opcode == 4 = compute program endIndex input 
+                        ((takeWithMode program (instructions !! 1) paraOneMode):output)
+    | opcode == 5 = if (jumpVal /= 0) 
+                        then compute program jumpToIndex input output
+                        else compute program endIndex input output
+    | opcode == 6 = if (jumpVal  == 0) 
+                        then compute program jumpToIndex  input output
+                        else compute program endIndex input output
+    | opcode == 7 = compute (replaceN program (instructions !!3) lessThan) 
+                        endIndex input output
+    | opcode == 8 = compute (replaceN program (instructions !!3) equals) 
+                        endIndex input output
+    | otherwise = [-1]
     where 
-        inputWODone = drop index input
-        paraOpt = head inputWODone
+        programWODone = drop index program
+        paraOpt = head programWODone
         opcode = paraOpt `mod` 100
         paraOneMode = paraOpt `div` 100 `mod` 10
         paraTwoMode = paraOpt `div` 1000 `mod` 10
         paraThreeMode = paraOpt `div` 10000 `mod` 10
-        numParas = case opcode of 
-                    99 -> 0
-                    1 -> 3
-                    2 -> 3
-                    3 -> 1
-                    4 -> 1
+        numParas = setNumParas opcode
         endIndex = index + numParas + 1
-        instructions = take (numParas + 1) inputWODone 
-        add = (takeWithMode input (instructions !! 1) paraOneMode) + 
-              (takeWithMode input (instructions !! 2) paraTwoMode)
-        multiply = (takeWithMode input (instructions !! 1) paraOneMode) * 
-                   (takeWithMode input (instructions !! 2) paraTwoMode)
+        instructions = take (numParas + 1) programWODone 
+        add = (takeWithMode program (instructions !! 1) paraOneMode) + 
+              (takeWithMode program (instructions !! 2) paraTwoMode)
+        multiply = (takeWithMode program (instructions !! 1) paraOneMode) * 
+                   (takeWithMode program (instructions !! 2) paraTwoMode)
+        jumpVal = (takeWithMode program (instructions !! 1) paraOneMode)
+        jumpToIndex = (takeWithMode program (instructions !! 2) paraTwoMode)
+        lessThan = if ((takeWithMode program (instructions !! 1) paraOneMode) < 
+                       (takeWithMode program (instructions !! 2) paraTwoMode)) then 1 else 0
+        equals = if ((takeWithMode program (instructions !! 1) paraOneMode) ==
+                      (takeWithMode program (instructions !! 2) paraTwoMode)) then 1 else 0
         
 takeWithMode :: [Int] -> Int -> Int -> Int
-takeWithMode input index paraMode 
-    | paraMode == 0 = input !! index
+takeWithMode program index paraMode 
+    | paraMode == 0 = program !! index
     | paraMode == 1 = index
     -- | otherwise = -1
 
@@ -49,3 +64,15 @@ replaceN (x:xs) n newVal
     | n == 0 = newVal:xs
     | otherwise = x: replaceN xs (n-1) newVal
 
+setNumParas :: Int -> Int
+setNumParas opcode
+    | opcode == 99 = 0
+    | opcode == 1 = 3
+    | opcode == 2 = 3
+    | opcode == 3 = 1
+    | opcode == 4 = 1
+    | opcode == 5 = 2
+    | opcode == 6 = 2
+    | opcode == 7 = 3
+    | opcode == 8 = 3
+    
