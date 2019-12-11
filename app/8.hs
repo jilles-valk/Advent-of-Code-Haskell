dcbe13491bc2368a743c1117ad589655162fe6be
Jilles Valk <jvalk@sogyo.nl>
2019-12-11 11:54:20 +0100

    Working part one of day eight.

---
+++ app/8.hs
@@ -0,0 +1,33 @@
+import System.IO
+import Data.List.Split
+
+main :: IO ()
+main = do 
+    handle <- openFile "data/input8.txt" ReadMode
+    contents <- hGetContents handle
+    let imgHeight = 6
+        imgWidth = 25
+        imgLayers = map (chunksOf imgWidth) $ chunksOf (imgWidth*imgHeight) contents 
+        fzLayer = fewestZeroLayers imgLayers [] (maxBound :: Int)
+        result = resultLayer fzLayer
+    putStrLn $ "The number of 1's times the number of 2's in the layer with the lowest number of 0's is: " 
+        ++ show result
+
+fewestZeroLayers :: [[String]] -> [[Char]] -> Int -> [[Char]]
+fewestZeroLayers (thisLayer:otherLayers) layer numZero
+    | null otherLayers = newLayer
+    | otherwise = fewestZeroLayers otherLayers newLayer newNumZero
+    where
+        thisNumZero = foldl (\count x -> count + countNumberAInList x '0') 0 thisLayer
+        thisLessZero = thisNumZero /= 0 && thisNumZero < numZero
+        newNumZero = if thisLessZero then thisNumZero else numZero
+        newLayer = if thisLessZero then thisLayer else layer
+
+countNumberAInList :: [Char] -> Char -> Int
+countNumberAInList list a = 
+    foldl (\count x -> if x == a then count + 1 else count) 0 list
+
+resultLayer :: [[Char]] -> Int
+resultLayer layer = 
+    (foldl (\count x -> count + countNumberAInList x '1') 0 layer) * 
+    (foldl (\count x -> count + countNumberAInList x '2') 0 layer)
