f34a1cacfca2b044fc7d5525e9cca81ccbf354f7
Jilles Valk <jvalk@sogyo.nl>
2020-01-02 13:41:05 +0100

    Working part two for day 12 by considering each axis seperately.

--- app/12.hs
+++ app/12.hs
@@ -14,40 +14,64 @@ main :: IO ()
 main = do
     let moons = Seq.fromList [(Moon 17 (-9) 4 0 0 0), (Moon 2 2 (-13) 0 0 0),
                           (Moon (-1) 5 (-1) 0 0 0), (Moon 4 7 (-7) 0 0 0)]
-    -- let moons = Seq.fromList [(Moon (-1) (0) (2) 0 0 0), (Moon (2) (-10) (-7) 0 0 0),
-    --                       (Moon (4) (-8) (8) 0 0 0), (Moon (3) (5) (-1) 0 0 0)]
         combis = Seq.fromList $ pairs $ [0 .. ((length moons) - 1)]
         simResMoons = runSim moons combis 1000
-   
-    putStrLn $ "The final positions and velocities are: "
-    putStrLn $ show simResMoons
+        (itForEqX, _) = findMoonSim moons moons combis 0 'x'
+        (itForEqY, _) = findMoonSim moons moons combis 0 'y'
+        (itForEqZ, _) = findMoonSim moons moons combis 0 'z'
+        itEq = lcm itForEqZ $ lcm itForEqX itForEqY
     putStrLn $ "The final total energy is: " ++ show (totalEngergy simResMoons 0)
+    putStrLn $ "Number of iterations to get back to initial state: " ++ show itEq
     
+findMoonSim :: Seq.Seq (Moon) -> Seq.Seq (Moon) -> Seq.Seq ((Int, Int)) -> Int -> Char -> (Int, Seq.Seq (Moon))
+findMoonSim initialMoons moons combis iteration axis
+    | initialMoons == moons && iteration /= 0 = (iteration, moons)
+    | otherwise = findMoonSim initialMoons newMoons combis (iteration + 1) axis
+    where 
+        moonsUpdateVel = updateVel moons combis axis
+        newMoons = updatePos moonsUpdateVel moonsUpdateVel 0 axis
 
 runSim :: Seq.Seq (Moon) -> Seq.Seq ((Int, Int)) -> Int -> Seq.Seq (Moon)
 runSim moons combis stepsLeft
     | stepsLeft == 0 = moons
     | Seq.null moons = Seq.empty
-    | otherwise = runSim (updatePos moonsUpdateVel moonsUpdateVel 0) combis (stepsLeft - 1)
+    | otherwise = runSim newMoons combis (stepsLeft - 1)
     where 
-        moonsUpdateVel = updateVel moons combis
+        moonsUpdateVel = updateVel moons combis 'a'
+        newMoons = updatePos moonsUpdateVel moonsUpdateVel 0 'a'
 
-updateVel :: Seq.Seq (Moon) -> Seq.Seq ((Int, Int)) -> Seq.Seq (Moon)
-updateVel moons combis 
+updateVel :: Seq.Seq (Moon) -> Seq.Seq ((Int, Int)) -> Char -> Seq.Seq (Moon)
+updateVel moons combis axis
     | Seq.null combis = moons
     | otherwise = do
         let (a, b) = Seq.index combis 0
             moonA = Seq.index moons a
             moonB = Seq.index moons b
-            newMoonA = Moon (xPos moonA) (yPos moonA) (zPos moonA) 
+            newMoonA = case axis of
+                'a' -> Moon (xPos moonA) (yPos moonA) (zPos moonA) 
                             (updateComponent (xPos moonA) (xPos moonB) (xVel moonA))
                             (updateComponent (yPos moonA) (yPos moonB) (yVel moonA))
                             (updateComponent (zPos moonA) (zPos moonB) (zVel moonA))
-            newMoonB = Moon (xPos moonB) (yPos moonB) (zPos moonB) 
+                'x' -> Moon (xPos moonA) (yPos moonA) (zPos moonA) 
+                            (updateComponent (xPos moonA) (xPos moonB) (xVel moonA))
+                            (yVel moonA) (zVel moonA)
+                'y' -> Moon (xPos moonA) (yPos moonA) (zPos moonA) (xVel moonA)
+                            (updateComponent (yPos moonA) (yPos moonB) (yVel moonA)) (zVel moonA)
+                'z' -> Moon (xPos moonA) (yPos moonA) (zPos moonA) (yVel moonA) (xVel moonA)
+                            (updateComponent (zPos moonA) (zPos moonB) (zVel moonA))
+            newMoonB = case axis of
+                'a' -> Moon (xPos moonB) (yPos moonB) (zPos moonB) 
                             (updateComponent (xPos moonB) (xPos newMoonA) (xVel moonB))
                             (updateComponent (yPos moonB) (yPos newMoonA) (yVel moonB))
                             (updateComponent (zPos moonB) (zPos newMoonA) (zVel moonB))
-        updateVel (Seq.update b newMoonB (Seq.update a newMoonA moons)) (Seq.drop 1 combis)
+                'x' -> Moon (xPos moonB) (yPos moonB) (zPos moonB) 
+                            (updateComponent (xPos moonB) (xPos moonA) (xVel moonB))
+                            (yVel moonB) (zVel moonB)
+                'y' -> Moon (xPos moonB) (yPos moonB) (zPos moonB) (xVel moonB)
+                            (updateComponent (yPos moonB) (yPos moonA) (yVel moonB)) (zVel moonB)
+                'z' -> Moon (xPos moonB) (yPos moonB) (zPos moonB) (yVel moonB) (xVel moonB)
+                            (updateComponent (zPos moonB) (zPos moonA) (zVel moonB))
+        updateVel (Seq.update b newMoonB (Seq.update a newMoonA moons)) (Seq.drop 1 combis) axis
 
 updateComponent :: Int -> Int -> Int -> Int
 updateComponent a b val = case compare a b of
@@ -55,17 +79,23 @@ updateComponent a b val = case compare a b of
                             GT -> val - 1
                             EQ -> val
 
-updatePos :: Seq.Seq (Moon) -> Seq.Seq (Moon) -> Int-> Seq.Seq (Moon)
-updatePos oldMoons newMoons index
+updatePos :: Seq.Seq (Moon) -> Seq.Seq (Moon) -> Int -> Char -> Seq.Seq (Moon)
+updatePos oldMoons newMoons index axis
     | Seq.null oldMoons = newMoons
-    | otherwise = updatePos (Seq.drop 1 oldMoons) (Seq.update index newMoon newMoons) (index + 1)
+    | otherwise = updatePos (Seq.drop 1 oldMoons) (Seq.update index newMoon newMoons) (index + 1) axis
     where 
         oldMoon = Seq.index oldMoons 0
-        newMoon = Moon (xPos oldMoon + xVel oldMoon)
+        newMoon = case axis of 
+            'a' -> Moon (xPos oldMoon + xVel oldMoon)
                        (yPos oldMoon + yVel oldMoon)
                        (zPos oldMoon + zVel oldMoon)
                        (xVel oldMoon) (yVel oldMoon) (zVel oldMoon)
-
+            'x' -> Moon (xPos oldMoon + xVel oldMoon) (yPos oldMoon) (zPos oldMoon) 
+                        (xVel oldMoon) (yVel oldMoon) (zVel oldMoon)
+            'y' -> Moon (xPos oldMoon) (yPos oldMoon + yVel oldMoon)
+                       (zPos oldMoon) (xVel oldMoon) (yVel oldMoon) (zVel oldMoon)
+            'z' -> Moon (xPos oldMoon) (yPos oldMoon) (zPos oldMoon + zVel oldMoon)
+                        (xVel oldMoon) (yVel oldMoon) (zVel oldMoon)
 
 pairs :: [a] -> [(a, a)]
 pairs xs =[ (x,y) | (x:rest) <- D.tails xs , y <- rest]
